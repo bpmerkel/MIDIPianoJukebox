768c977fe0ee4e2f3db7a4f9657cb48989f06dce
Brady P. Merkel <brady@merkel.net>
2023-05-20 22:14:27 -0400

    Refactor refresh

--- Data/JukeboxService.cs
+++ Data/JukeboxService.cs
@@ -1,27 +1,40 @@
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
 namespace MIDIPianoJukebox.Data;
 
-public sealed partial class JukeboxService
+public class JukeboxService: IDisposable
 {
     const string cxstring = "Filename=jukebox.db";
-    private Jukebox jukebox;
-    private readonly object syncroot = new object();
+    public bool Loaded { get; set; } = false;
+    private readonly object syncroot = new();
+    public Settings Settings { get; set; } = new();
+    public List<Playlist> Playlists { get; private set; }
+    public List<Tune> Tunes { get; set; }
+    public List<Tune> Queue { get; } = new();
+    public static List<string> Log { get; } = new();
+    public Tune Tune { get; set; }
+    public int TotalTime { get; set; }
+    public TimeSpan CurrentTime { get; set; }
+    public TimeSpan RemainingTime { get; set; }
+    public MidiPlayer CurrentPlayer { get; set; }
+    public PlayerState State => CurrentPlayer?.State ?? PlayerState.Stopped;
+    public Action<double> ProgressChanged { get; set; }
 
-    public static List<string> Log { get; } = new List<string>();
+    private double _progress = 0d;
+    public double Progress
+    {
+        get => _progress;
+        set
+        {
+            _progress = value;
+            ProgressChanged(value);
+        }
+    }
 
-    public Task<Jukebox> GetJukeboxAsync()
+    public Task GetJukeboxAsync()
     {
         return Task.Run(() =>
         {
-            if (jukebox != null) return jukebox;
             lock (syncroot)
             {
-                if (jukebox != null) return jukebox;
-
                 using var db = new LiteDatabase(cxstring);
                 var tunes = db.GetCollection<Tune>();
                 var playlists = db.GetCollection<Playlist>();
@@ -37,8 +50,10 @@ public sealed partial class JukeboxService
                     playlists.Insert(new Playlist { Name = "Favorites", ID = ObjectId.NewObjectId() });
                 }
 
-                jukebox = new Jukebox(playlists.FindAll(), tunes.FindAll(), settings.FindOne(q => true));
-                return jukebox;
+                Playlists = playlists.Include(p => p.Tunes).FindAll().Where(p => p != null).OrderBy(p => p.Name).ToList();
+                Tunes = tunes.FindAll().Where(t => t != null).OrderBy(t => t.Name).ToList();
+                Settings = settings.FindOne(q => true);
+                Loaded = true;
             }
         });
     }
@@ -49,7 +64,7 @@ public sealed partial class JukeboxService
     {
         return Task.Run(() =>
         {
-            logger($"Cleansing database {cxstring} for MIDI files in {jukebox.Settings.MIDIPath}");
+            logger($"Cleansing database {cxstring} for MIDI files in {Settings.MIDIPath}");
             using var db = new LiteRepository(cxstring);
 
             progress(.1d);
@@ -57,10 +72,10 @@ public sealed partial class JukeboxService
             var todelete = db.Query<Tune>()
                 .ToEnumerable()
                 .AsParallel()
-                .Where(tune => !File.Exists(Path.Combine(jukebox.Settings.MIDIPath, tune.Filepath)))
+                .Where(tune => !File.Exists(Path.Combine(Settings.MIDIPath, tune.Filepath)))
                 .ToList();
 
-            logger($"Found {todelete.Count:#,##0} Tunes that no longer exists under {jukebox.Settings.MIDIPath}");
+            logger($"Found {todelete.Count:#,##0} Tunes that no longer exists under {Settings.MIDIPath}");
 
             progress(.5d);
 
@@ -77,59 +92,60 @@ public sealed partial class JukeboxService
 
                 db.Database.Rebuild();
 
-                jukebox.Tunes = db.Query<Tune>().ToList();
+                Tunes = db.Query<Tune>().ToList();
             }
             progress(1d);
             logger("Database cleanse complete");
         });
     }
 
-    public Task RefreshDatabase(Action<string> logger, Action<double> progress)
+    public Task RefreshDatabaseAsync(Action<string> logger, Action<double> progress)
     {
         return Task.Run(() =>
         {
-            logger($"Updating database {cxstring} from MIDI files in {jukebox.Settings.MIDIPath}");
-            var files = new DirectoryInfo(jukebox.Settings.MIDIPath)
+            logger($"Updating database {cxstring} from MIDI files in {Settings.MIDIPath}");
+            var files = new DirectoryInfo(Settings.MIDIPath)
                 .EnumerateFiles("*.mid", SearchOption.AllDirectories)
                 .ToList();
 
-            var total = (float)files.Count;
+            var total = files.Count;
             logger($"Processing {total:#,##0} MIDI files");
 
-                // use the top-level folder name as the Genre
-                // pre-pend the immediate folder name in the name
-                // use the file name as the name
-                // capture the file path too
+            // use the top-level folder name as the Genre
+            // pre-pend the immediate folder name in the name
+            // use the file name as the name
+            // capture the file path too
 
-                var re1 = new Regex(@"[\\\'\/\>\<;:\|\*?\@\=\^\!\`\~\#\u0000\u0001\u0003\u0004]", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            var re1 = new Regex(@"[\\\'\/\>\<;:\|\*?\@\=\^\!\`\~\#\u0000\u0001\u0003\u0004]", RegexOptions.IgnoreCase | RegexOptions.Compiled);
             var re2 = new Regex(@"[\s_-]+|\.$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
             var reIgnore = new Regex(@"^(untitled|generated|played|written|words|\(brt\)|Copyright|http|www\.|e?mail|piano|acoustic|pedal|edition|sequenced|music\s+by|for\s+general|by\s+|words\s+|from\s+|arranged\s+|sung\s+|composed|dedicated|kmidi|melody|seq|track|this\s+and|1800S|midi\s+out|\S+\.com|\S+\.org|All Rights Reserved|with|when|just|Bdca426|dont|know|some|what|like|this|tk10|youre|Bwv001|Unnamed|comments|have|will|thing|come|v0100|midisource)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
             var counter = 0;
-            var sw = Stopwatch.StartNew();
 
             using var db = new LiteRepository(cxstring);
             files.AsParallel().ForAll(file =>
             {
-                var subpath = file.FullName.Substring(jukebox.Settings.MIDIPath.Length + 1);
+                var subpath = file.FullName.Substring(Settings.MIDIPath.Length + 1);
                 var library = subpath.Contains("\\") ? subpath.Substring(0, subpath.IndexOf("\\")) : subpath;
                 var name = Path.GetFileNameWithoutExtension(file.Name);
                 var tags = new List<string> { Path.GetFileName(file.DirectoryName), library };
                 var tracksCount = 0;
+                var messagesCount = 0;
+                var eventsCount = 0;
+                var complexity = 0;
                 var duration = 0;
 
                 try
                 {
-                    using var str = file.OpenRead();
-                    var music = MidiMusic.Read(str);
+                    var music = GetMusic(file.FullName);
                     duration = music.GetTotalPlayTimeMilliseconds();
-
-                    var events = music.Tracks
-                        .SelectMany(track => track.Messages)
-                        .Select(msg => msg.Event)
-                        .ToList();
-
                     tracksCount = music.Tracks.Count;
+                    var messages = music.Tracks.SelectMany(track => track.Messages).ToList();
+                    messagesCount = messages.Count;
+
+                    var events = messages.Select(msg => msg.Event).ToList();
+                    eventsCount = events.Count;
+                    complexity = tracksCount * eventsCount / messagesCount;
 
                     tags.AddRange(events
                         .Where(e => e.EventType == MidiEvent.Meta && (e.Msb == MidiMetaType.TrackName || e.Msb == MidiMetaType.Text || e.Msb == MidiMetaType.InstrumentName))
@@ -145,43 +161,43 @@ public sealed partial class JukeboxService
                     return;
                 }
 
-                    // add/update the tune
-                    var tune = db.Query<Tune>()
+                // add/update the tune
+                var tune = db.Query<Tune>()
                     .Where(t => t.Filepath.Equals(subpath, StringComparison.CurrentCultureIgnoreCase))
                     .FirstOrDefault()
                     ?? new Tune();
                 tune.Name = name;
+                tune.AddedUtc = DateTime.UtcNow;
                 tune.Library = library;
                 tune.Filepath = subpath;
                 tune.Tracks = tracksCount;
+                tune.Messages = messagesCount;
+                tune.Events = eventsCount;
+                tune.Complexity = complexity;
                 tune.Durationms = duration;
                 tune.Tags = tags
                     .Where(m => !string.IsNullOrWhiteSpace(m))
                     .Where(m => m.Length > 3)
-                    .Select(m => toTitleCase(m))
+                    .Select(m => ToTitleCase(m))
                     .OrderBy(m => m)
                     .Distinct()
                     .ToList();
+                db.Upsert(tune);
 
-                if (tune.Filepath != null)
-                {
-                    db.Upsert(tune);
-                }
-
+                var sw = Stopwatch.StartNew();
                 Interlocked.Increment(ref counter);
                 if (counter % 500 == 0 || counter == total)
                 {
                     var rate = counter / (float)sw.ElapsedMilliseconds; // = items per ms
-                        var msleft = (total - counter) / rate; // = ms
-                        var eta = DateTime.Now.AddMilliseconds(msleft);
-                    logger($"{(total - counter):#,##0} = {(counter / total):P0} eta: {eta:h:mm:ss} {subpath} => {library} / {name}");
+                    var msleft = (total - counter) / rate; // = ms
+                    var eta = DateTime.Now.AddMilliseconds(msleft);
+                    logger($"{(total - counter):#,##0} = {(100f * counter / total):##0}% eta: {eta:h:mm:ss} {library} / {name}");
                     progress(counter / total);
                 }
             });
 
             logger("Shrinking Database...");
             db.Database.Rebuild();
-            jukebox.Tunes = db.Query<Tune>().ToList();
             logger("Database refresh complete");
         });
     }
@@ -189,7 +205,7 @@ public sealed partial class JukeboxService
     public void SaveSettings()
     {
         using var db = new LiteRepository(cxstring);
-        db.Upsert(jukebox.Settings);
+        db.Upsert(Settings);
     }
 
     public void SaveTune(Tune tune)
@@ -220,10 +236,10 @@ public sealed partial class JukeboxService
     {
         if (!string.IsNullOrWhiteSpace(playlistName))
         {
-            var playlist = jukebox.Playlists.FirstOrDefault(p => p.Name.Equals(playlistName, StringComparison.CurrentCultureIgnoreCase));
+            var playlist = Playlists.FirstOrDefault(p => p.Name.Equals(playlistName, StringComparison.CurrentCultureIgnoreCase));
             if (playlist != null)
             {
-                jukebox.Playlists.Remove(playlist);
+                Playlists.Remove(playlist);
                 using var db = new LiteRepository(cxstring);
                 db.Delete<Playlist>(playlist.ID);
             }
@@ -236,28 +252,27 @@ public sealed partial class JukeboxService
         // interrupt any current playing Tune
         StopPlayer();
 
-        if (jukebox.Queue.Any())
+        if (Queue.Any())
         {
             // get the next queue item
-            var idx = shuffle ? new Random().Next(0, jukebox.Queue.Count - 1) : 0;
-            jukebox.Current.Tune = jukebox.Queue[idx];
-            jukebox.Queue.RemoveAt(idx);
-            SaveTune(jukebox.Current.Tune);
+            var idx = shuffle ? new Random().Next(0, Queue.Count - 1) : 0;
+            Tune = Queue[idx];
+            Queue.RemoveAt(idx);
 
             // play 'next'
             PlayPlayer();
         }
         else
         {
-            jukebox.Current.Tune = null;
+            Tune = null;
         }
     }
 
     public void Enqueue(Tune tune)
     {
-        if (tune != null && !jukebox.Queue.Contains(tune))
+        if (tune != null && !Queue.Contains(tune))
         {
-            jukebox.Queue.Add(tune);
+            Queue.Add(tune);
         }
     }
 
@@ -267,32 +282,189 @@ public sealed partial class JukeboxService
         {
             foreach (var tune in playlist.Tunes)
             {
-                jukebox.Queue.Add(tune);
+                Queue.Add(tune);
             }
         }
     }
 
     public void Dequeue(Tune tune)
     {
-        if (tune != null && jukebox.Queue.Contains(tune))
+        if (tune != null && Queue.Contains(tune))
         {
-            jukebox.Queue.Remove(tune);
+            Queue.Remove(tune);
         }
     }
 
     public void EnqueueAll(List<Tune> tunes)
     {
-        jukebox.Queue.AddRange(tunes);
+        Queue.AddRange(tunes);
     }
 
     public void DequeueAll()
     {
-        if (jukebox.Queue.Any())
+        if (Queue.Any())
         {
-            jukebox.Queue.Clear();
+            Queue.Clear();
         }
     }
 
-    private static readonly TextInfo textInfo = Thread.CurrentThread.CurrentCulture.TextInfo;
-    private static string toTitleCase(string input) => textInfo.ToTitleCase(input.ToLower());
+    private IMidiOutput outputDevice;
+    public event EventHandler ReadyToPlayNext;
+    readonly SynchronizationContext main = SynchronizationContext.Current;
+    bool isStopping;
+
+    private void PlayPlayer()
+    {
+        // spin wait until stopped, if pending a stop
+        while (isStopping) Thread.Sleep(100);
+
+        if (CurrentPlayer == null)
+        {
+            var music = GetMusic();
+            if (music != null)
+            {
+                outputDevice = getDevice();
+                CurrentPlayer = new MidiPlayer(music, outputDevice);
+                TotalTime = CurrentPlayer.GetTotalPlayTimeMilliseconds();
+                CurrentTime = TimeSpan.FromMilliseconds(0);
+                RemainingTime = TimeSpan.FromMilliseconds(TotalTime);
+                CurrentPlayer.PlaybackCompletedToEnd += player_Finished;
+                CurrentPlayer.EventReceived += player_EventReceived;
+                CurrentPlayer.Play();
+            }
+            else
+            {
+                // found a missing/bad file; so signal move ahead
+                signal_next();
+            }
+        }
+    }
+
+    private void player_Finished()
+    {
+        // will need this to signal next tune in the queue
+        Tune.Plays += 1;
+        signal_next();
+    }
+
+    private void signal_next()
+    {
+        // will need this to signal next tune in the queue
+        main.Post(state => ReadyToPlayNext?.Invoke(this, new EventArgs()), null);
+    }
+
+    private void player_EventReceived(MidiEvent m)
+    {
+        if (CurrentPlayer == null) return;
+        if (CurrentPlayer.State != PlayerState.Playing) return;
+
+        // be aware, if user does play next when event is firing, player may come back as null...
+        // a.EventType, a.Channel, a.StatusByte, a.Value
+        var prior = $"{Progress:P0}-{CurrentTime.TotalSeconds:#}";
+        var time = CurrentPlayer?.PositionInTime.TotalMilliseconds ?? 0;
+        var progress = time / (TotalTime + 1);
+        CurrentTime = TimeSpan.FromMilliseconds(time);
+        RemainingTime = TimeSpan.FromMilliseconds(TotalTime - time);
+
+        var newp = $"{progress:P0}-{CurrentTime.TotalSeconds:#}";
+        if (prior != newp) Progress = progress;  // which also signals a UI update
+    }
+
+    private void StopPlayer()
+    {
+        if (CurrentPlayer == null) return;
+        if (isStopping)
+        {
+            // spin wait until stopped
+            while (isStopping) Thread.Sleep(100);
+        }
+        else
+        {
+            isStopping = true;
+            CurrentPlayer.Stop();
+            // spin wait until stopped
+            while (CurrentPlayer.State != PlayerState.Stopped) Thread.Sleep(100);
+            CurrentPlayer.EventReceived -= player_EventReceived;
+            CurrentPlayer.PlaybackCompletedToEnd -= player_Finished;
+            Progress = 0;
+            CurrentTime = TimeSpan.FromMilliseconds(0);
+            RemainingTime = TimeSpan.FromMilliseconds(TotalTime);
+            CurrentPlayer.Dispose();
+            outputDevice.Dispose();
+            outputDevice = null;
+            CurrentPlayer = null;
+            isStopping = false;
+        }
+    }
+
+    public void PausePlayer()
+    {
+        if (CurrentPlayer == null) return;
+        if (CurrentPlayer.State != PlayerState.Paused)
+        {
+            try
+            {
+                CurrentPlayer.Pause();
+            }
+            catch (Win32Exception) { }
+        }
+    }
+
+    public void ResumePlayer()
+    {
+        if (CurrentPlayer == null) return;
+        if (CurrentPlayer.State != PlayerState.Playing)
+        {
+            CurrentPlayer.Play();
+        }
+    }
+
+    public void SkipPlayer(int ticks)
+    {
+        if (CurrentPlayer == null) return;
+        SkipPlayerTo(CurrentPlayer.PlayDeltaTime + ticks);
+    }
+
+    // perform an absolute skip
+    public void SkipPlayerTo(int ticks)
+    {
+        if (CurrentPlayer == null || ticks < 0) return;
+        CurrentPlayer.Seek(ticks);
+        ResumePlayer();
+    }
+
+    private MidiMusic GetMusic() => GetMusic(Path.Combine(Settings.MIDIPath, Tune.Filepath));
+    private MidiMusic GetMusic(string path)
+    {
+        try
+        {
+            using var sr = File.OpenRead(path);
+            return MidiMusic.Read(sr);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public List<IMidiPortDetails> GetDevices()
+    {
+        var wmm = new WinMMMidiAccess();
+        return wmm.Outputs.ToList();
+    }
+
+    public IMidiOutput getDevice()
+    {
+        var wmm = new WinMMMidiAccess();
+        var dev = wmm.OpenOutputAsync(Settings.OutputDevice ?? "0").Result;
+        return dev;
+    }
+
+    public void Dispose()
+    {
+        if (CurrentPlayer != null) StopPlayer();
+        GC.SuppressFinalize(this);
+    }
+
+    private static string ToTitleCase(string input) => Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
 }
--- Data/Model.cs
+++ Data/Model.cs
@@ -12,18 +12,12 @@ public class Tune : IEqualityComparer<Tune>
     public float Rating { get; set; }
     public int Durationms { get; set; }
     public int Tracks { get; set; }
+    public int Messages { get; set; }
+    public int Events { get; set; }
+    public int Complexity { get; set; }
     [BsonIgnore] public TimeSpan Duration => TimeSpan.FromMilliseconds(Durationms);
-
     public bool Equals([AllowNull] Tune x, [AllowNull] Tune y) => x?.ID == y?.ID;
     public int GetHashCode([DisallowNull] Tune obj) => obj?.ID.GetHashCode() ?? 0;
-}
-
-public class Library : IEqualityComparer<Library>
-{
-    public string Name { get; set; }
-    public List<Tune> Tunes { get; set; } = new List<Tune>();
-    public bool Equals([AllowNull] Library x, [AllowNull] Library y) => x?.Name == y?.Name;
-    public int GetHashCode([DisallowNull] Library obj) => obj?.Name.GetHashCode(StringComparison.CurrentCultureIgnoreCase) ?? 0;
 }
 
 public class Playlist : IEqualityComparer<Playlist>
@@ -40,61 +34,14 @@ public class Playlist : IEqualityComparer<Playlist>
 public class Settings
 {
     [BsonId] public ObjectId ID { get; set; } = ObjectId.NewObjectId();
-    public string MIDIPath { get; set; } = @"e:\MIDI";
-    public string OutputDevice { get; set; } = "0";
+    public string MIDIPath { get; set; } = @"d:\MIDI";
+    public string OutputDevice { get; set; } = "2";
 }
 
-public enum States { Playing, Paused, Stopped }
-
-public class Current
+public class Library : IEqualityComparer<Library>
 {
-    public Tune Tune { get; set; }
-    public States State { get; set; } = States.Stopped;
-    public int TotalTime { get; set; }
-    public TimeSpan CurrentTime { get; set; }
-    public TimeSpan RemainingTime { get; set; }
-
-    public event EventHandler ProgressChanged;
-
-    private double _progress;
-    public double Progress
-    {
-        get => _progress;
-        set
-        {
-            _progress = value;
-            ProgressChanged?.Invoke(this, new EventArgs());
-        }
-    }
-}
-
-public class Jukebox
-{
-    public Current Current { get; } = new Current();
-    public Settings Settings { get; set; } = new Settings();
-    public List<Playlist> Playlists { get; private set; }
-    public List<Library> Libraries { get; private set; }
-    public List<Tune> Queue { get; } = new List<Tune>();
-
-    private List<Tune> _tunes;
-    public List<Tune> Tunes
-    {
-        get => _tunes;
-        set
-        {
-            _tunes = value;
-            Libraries = _tunes
-                .GroupBy(t => t.Library)
-                .Select(g => new Library { Name = g.Key, Tunes = g.ToList() })
-                .OrderBy(g => g.Name)
-                .ToList();
-        }
-    }
-
-    public Jukebox(IEnumerable<Playlist> playlists, IEnumerable<Tune> tunes, Settings settings)
-    {
-        Playlists = playlists.OrderBy(p => p.Name).ToList();
-        Tunes = tunes.OrderBy(t => t.Name).ToList();
-        if (settings != null) Settings = settings;
-    }
+    public string Name { get; set; }
+    public List<Tune> Tunes { get; set; } = new List<Tune>();
+    public bool Equals([AllowNull] Library x, [AllowNull] Library y) => x?.Name == y?.Name;
+    public int GetHashCode([DisallowNull] Library obj) => obj?.Name.GetHashCode(StringComparison.CurrentCultureIgnoreCase) ?? 0;
 }
